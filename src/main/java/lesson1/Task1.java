1181484777413afbcdd95eaad6c3b8d73c3afcb3
Tetiana Snytko <lisitsina070295@gmail.com>
2019-09-21 20:41:01 +0300

    finished task_1

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -5,10 +5,17 @@ public class Task1 {
     Написать функцию, которая вычисляет минимум из трёх чисел.
     */
     public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
+        System.out.println(min(5, 2, 3));
     }
 
     public static int min(int a, int b, int c) {
-        return 0;
+        return min(min(a, b), c);
+    }
+
+    public static int min(int a, int b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
     }
 }
